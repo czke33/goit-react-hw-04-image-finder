b977dc93744eb4a8e157c891ccb1c4922cb731c4
czke33 <czke33@gmail.com>
2024-04-23 12:29:28 +0300

    update app with useState useEffect hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect} from 'react';
 import './App.module.css';
 import api from '../services/api';
 import Searchbar from './Searchbar/Searchbar';
@@ -8,134 +8,118 @@ import Loader from './Loader/Loader';
 import Modal from './Modal/Modal';
 import OnError from './OnError/OnError';
 
-class App extends Component {
-  state = {
-    images: [],
-    isLoading: false,
-    error: null,
-    query: '',
-    actualPage: 1,
-    lastPage: 1,
-    modalIsOpen: false,
-  };
+const App = () => {
+	const [images, setImages] = useState([]);
+	const [isLoading, setIsLoading] = useState(false);
+	const [query, setQuery] = useState("");
+	const [actualPage, setActualPage] = useState(1);
+	const [lastPage, setLatPage] = useState(1);
+	const [modalIsOpen, setModalIsOpen] = useState(false);
+	const [modalPhotoURL, setModalPhotoURL] = useState(null);
+	const [modalAlt, setModalAlt] = useState(null);
 
-  modalInfo = {
-    modalPhotoURL: null,
-    modalAlt: null,
-  };
+	const updateQuery = ({ query }) => {
+		setQuery(query);
+	};
 
-  updateQuery = ({ query }) => {
-    this.setState({ query: query });
-  };
+	const mapNewImages = (fetchedImages) => {
+		const mapedImages = fetchedImages.map((image) => ({
+			id: image.id,
+			small: image.webformatURL,
+			large: image.largeImageURL,
+			alt: image.tags,
+		}));
+		return mapedImages;
+	};
 
-  mapNewImages = (fetchedImages) => {
-    const mapedImages = fetchedImages.map((image) => ({
-      id: image.id,
-      small: image.webformatURL,
-      large: image.largeImageURL,
-      alt: image.tags,
-    }));
-    return mapedImages;
-  };
+	const goToNextPage = () => {
+		setActualPage(actualPage + 1);
+	};
 
-  goToNextPage = () => {
-    let { actualPage } = this.state;
-    actualPage++;
-    this.setState({ actualPage: actualPage });
-  };
+	const openModal = (e) => {
+		setModalPhotoURL(e.target.dataset["source"]);
+		setModalAlt(e.target.alt);
+		setModalIsOpen(true);
+	};
 
-  openModal = (e) => {
-    this.setState({
-      modalIsOpen: true,
-    });
+	const closeModal = (e) => {
+		if (e.target.nodeName !== "IMG") {
+			setModalIsOpen(false);
+		}
+	};
 
-    this.modalInfo = {
-      modalPhotoURL: e.target.dataset['source'],
-      modalAlt: e.target.alt,
-    };
-  };
+	const closeModalwithButton = (e) => {
+		if (e.key === "Escape") {
+			setModalIsOpen(false);
+		}
+	};
 
-  closeModal = (e) => {
-    if (e.target.nodeName !== 'IMG') {
-      this.setState({
-        modalIsOpen: false,
-      });
-    }
-  };
+	const fetchWitchQuery = async () => {
+		try {
+			setIsLoading(true);
+			const fetchedData = await api.fetchImageWithQuery(query, 1);
+			const mapedImages = await mapNewImages(fetchedData.images);
+			const lastPage = Math.ceil(fetchedData.total / 12);
+			setActualPage(1);
+			setImages([...mapedImages]);
+			setLatPage(lastPage);
+			window.scrollTo({ top: 0, behavior: "smooth" });
+		} catch (error) {
+			console.log("Error");
+		} finally {
+			setIsLoading(false);
+		}
+	};
 
-  closeModalwithButton = (e) => {
-    if (e.key === 'Escape') {
-      this.setState({
-        modalIsOpen: false,
-      });
-    }
-  };
+	useEffect(() => {
+		if (query !== "") {
+			fetchWitchQuery();
+		}
+	}, [query]);
 
-  async componentDidUpdate(prevProps, prevState) {
-    if (prevState.query !== this.state.query) {
-      const { query } = this.state;
-      this.setState({ isLoading: true });
-      try {
-        const fetchedData = await api.fetchImageWithQuery(query, 1);
-        const mapedImages = this.mapNewImages(fetchedData.images);
-        const lastPage = Math.ceil(fetchedData.total / 12);
-        this.setState({ images: mapedImages, actualPage: 1, lastPage: lastPage });
-        window.scrollTo({ top: 0, behavior: 'smooth' });
-      } catch (error) {
-        this.setState({ error });
-      } finally {
-        this.setState({ isLoading: false });
-      }
-    }
+	const fetchWithButton = async () => {
+		try {
+			setIsLoading(true);
+			const fetchedData = await api.fetchImageWithQuery(query, actualPage);
+			const mapedImages = await mapNewImages(fetchedData.images);
+			const concatImages = images.concat(mapedImages);
+			setImages([...concatImages]);
+		} catch (error) {
+			console.log("Error");
+		} finally {
+			setIsLoading(false);
+		}
+	};
 
-    if (
-      prevState.actualPage !== this.state.actualPage &&
-      prevState.query === this.state.query &&
-      this.state.actualPage !== 1
-    ) {
-      const { query, actualPage, images } = this.state;
-      this.setState({ isLoading: true });
-      try {
-        const fetchedData = await api.fetchImageWithQuery(query, actualPage);
-        const mapedImages = await this.mapNewImages(fetchedData.images);
-        const concatImages = images.concat(mapedImages);
-        this.setState({ images: concatImages });
-      } catch (error) {
-        this.setState({ error });
-      } finally {
-        this.setState({ isLoading: false });
-      }
-    }
-  }
+	useEffect(() => {
+		if (actualPage !== 1) {
+			fetchWithButton();
+		}
+	}, [actualPage]);
 
-  render() {
-    const { images, actualPage, lastPage, isLoading, modalIsOpen, query } = this.state;
-    const { modalPhotoURL, modalAlt } = this.modalInfo;
-
-    return (
-      <>
-        {modalIsOpen && (
-          <Modal
-            imgSrc={modalPhotoURL}
-            imgAlt={modalAlt}
-            closeHandler={this.closeModal}
-            escHandler={this.closeModalwithButton}
-          ></Modal>
-        )}
-        <Searchbar onSubmit={this.updateQuery} />
-        <ImageGallery
-          images={images}
-          page={actualPage}
-          clickHanlder={this.openModal}
-        ></ImageGallery>
-        {actualPage !== lastPage && images.length > 0 && !isLoading ? (
-          <Button onClick={this.goToNextPage} />
-        ) : null}
-        {isLoading && <Loader />}
-        {images.length === 0 && query && !isLoading && <OnError>Nothing found! Try again</OnError>}
-      </>
-    );
-  }
-}
+	return (
+		<>
+			{modalIsOpen && (
+				<Modal
+					src={modalPhotoURL}
+					alt={modalAlt}
+					closeHandler={closeModal}
+					escHandler={closeModalwithButton}
+				></Modal>
+			)}
+			<Searchbar onSubmit={updateQuery} />
+			<ImageGallery images={images} page={actualPage} clickHanlder={openModal} />
+			{actualPage !== lastPage && images.length > 0 && isLoading === false ? (
+				<Button onClick={goToNextPage} />
+			) : (
+				""
+			)}
+			{isLoading && <Loader />}
+			{images.length === 0 && query !== "" && isLoading === false && (
+				<OnError>Nothing found! Try again</OnError>
+			)}
+		</>
+	);
+};
 
 export default App;
